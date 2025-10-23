044968535afaf417dc62b171910d915bd5158d06
esmeejellema <esmeejellema@hotmail.com>
2025-10-23 17:41:25 +0200

    initial commit

---
+++ src/main/java/com/esmee/poppick_backend/PoppickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.esmee.poppick_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoppickBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoppickBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esmee/poppick_backend/PoppickBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esmee.poppick_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppickBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
